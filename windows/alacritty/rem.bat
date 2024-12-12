76e5d404395b89281de8a6faa4db78bde032e28d
Dv <75692623+kyaruwo@users.noreply.github.com>
2024-12-12 21:07:28 +0800

    kill :>

--- windows/alacritty/rem.bat
+++ windows/alacritty/rem.bat
@@ -1,4 +1,8 @@
 taskkill /F /IM alacritty.exe
-
+taskkill /F /IM alacritty.exe
+taskkill /F /IM alacritty.exe
+taskkill /F /IM alacritty.exe
+:: :>
 del alacritty.url
+del %appdata%\alacritty\alacritty.exe
 rmdir %appdata%\alacritty /s /q
--- windows/syncthing/rem.bat
+++ windows/syncthing/rem.bat
@@ -1,4 +1,8 @@
 taskkill /F /IM syncthing.exe
-
+taskkill /F /IM syncthing.exe
+taskkill /F /IM syncthing.exe
+taskkill /F /IM syncthing.exe
+:: :>
 del gui.url
+del %appdata%\syncthing\syncthing.exe
 rmdir %appdata%\syncthing /s /q
