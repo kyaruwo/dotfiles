a2ca1fb0a7b5b580f83b971229c588743f162b05
Dv <75692623+kyaruwo@users.noreply.github.com>
2025-11-16 01:44:31 +0800

    syncthing-v2.0.11

--- syncthing/init.bat
+++ syncthing/init.bat
@@ -1,9 +1,9 @@
 if not exist %appdata%\syncthing (
     mkdir %appdata%\syncthing
-    curl -L -o %appdata%\syncthing\syncthing.zip https://github.com/syncthing/syncthing/releases/download/v1.28.1/syncthing-windows-amd64-v1.28.1.zip
+    curl -L -o %appdata%\syncthing\syncthing.zip https://github.com/syncthing/syncthing/releases/download/v2.0.11/syncthing-windows-amd64-v2.0.11.zip
     powershell.exe Expand-Archive -Path %appdata%\syncthing\syncthing.zip -DestinationPath %appdata%\syncthing
-    xcopy %appdata%\syncthing\syncthing-windows-amd64-v1.28.1 %appdata%\syncthing /s
-    rmdir %appdata%\syncthing\syncthing-windows-amd64-v1.28.1 /s /q
+    xcopy %appdata%\syncthing\syncthing-windows-amd64-v2.0.11 %appdata%\syncthing /s
+    rmdir %appdata%\syncthing\syncthing-windows-amd64-v2.0.11 /s /q
     del %appdata%\syncthing\syncthing.zip
 
     echo [InternetShortcut]> "(web)GUI.url"
